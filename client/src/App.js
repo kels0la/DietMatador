fa20957079dcd168967360932a0577a79abea2c1
kels0la <samelsola@gmail.com>
2019-02-09 16:04:43 -0600

    configuration and initial setup

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        hello
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
