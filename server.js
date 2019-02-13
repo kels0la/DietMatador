c83618ed4dfc1f01b3c72c4d979b017414dafb88
Trie0037 <trie0037@gmail.com>
2019-02-13 17:32:38 -0600

    cssbanner

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
+import express, { static } from 'express';
+import { urlencoded, json } from 'body-parser';
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(static('public'));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(static('client/build'));
 }
 
 // Start the API server
