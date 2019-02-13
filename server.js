ea35fdaff4cdf4f404906c535d5f455df3f94565
kels0la <samelsola@gmail.com>
2019-02-13 17:49:38 -0600

    server fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
 // const admin = require('firebase-admin');
+// testing
 const app = express();
 
 const PORT = process.env.PORT || 3001;
